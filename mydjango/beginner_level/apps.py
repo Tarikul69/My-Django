9d691b31b4528dc62e8037ba4a71c838fbc8ff97
Tarikul69 <tarikulabir69@gmail.com>
2025-07-21 14:00:12 +0600

    Level added

---
+++ mydjango/beginner_level/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BeginnerLevelConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'beginner_level'
---
+++ mydjango/intermediate_level/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IntermediateLevelConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'intermediate_level'
