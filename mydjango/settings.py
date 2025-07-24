8864e6e0def656e2e42bf4e3204d7fdf106d46b8
Tarikul69 <tarikulabir69@gmail.com>
2025-07-24 17:31:46 +0600

    Change directory

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,7 +79,19 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+####################################################
+#########Database Configuration##########
+####################################################
+DATABASES = {
+    'default': {
+        'ENGINE': config('ENGINE'),  # or your DB engine
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
